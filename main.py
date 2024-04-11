7bbe60bacad0a4212632c116295054bb37a8bfef
davincent <dgvincent95@gmail.com>
2024-04-10 19:54:33 -0500

    bookbot now counts individual letters too

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
+    num_letters = get_num_letters(text)
     print(f"{num_words} words found in the document")
+    print(num_letters)
 
 
 def get_num_words(text):
@@ -13,5 +15,14 @@ def get_num_words(text):
 def get_book_text(path):
     with open(path) as f:
         return f.read()
+
+def get_num_letters(text):
+    lowercase_text = text.lower()
+    num_letter_dict = {}
+    letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    for letter in letters:
+        num_letter_dict[letter] = lowercase_text.count(letter)
+    return num_letter_dict
+
 
 main()
